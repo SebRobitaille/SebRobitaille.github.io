93bee5fcb1026b35997e17524ca9e48a895c5e08
Sebastian Robitaille <95282730+SebRobitaille@users.noreply.github.com>
2022-12-18 01:13:10 -0800

    christmas version

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-const song = document.getElementById("song")
-song.play()
+// create a function to generate a snowflake element
+function createSnowflake() {
+  const snowflake = document.createElement("div")
+  snowflake.classList.add("snowflake")
+  snowflake.style.left = `${Math.random() * 100}vw`
+  return snowflake
+}
+
+// select the container element where the snowflakes will be added
+const container = document.querySelector(".snowfall-container")
+
+// create and append 100 snowflake elements to the container
+// for (let i = 0; i < 100; i++) {
+//   container.appendChild(createSnowflake())
+// }
+
+setInterval(() => {
+  const snowflake = createSnowflake()
+  container.appendChild(snowflake)
+  setTimeout(() => {
+    snowflake.remove()
+  }, 5000)
+}, 100)
